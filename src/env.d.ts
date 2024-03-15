a74d5e8c4cd6b7885260e91dbd2ac936fcaed498
Pablo Peña <pablo.sebastian.pena@gmail.com>
2024-03-15 15:34:40 -0300

    Add env and global types

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly MONGODB_URI: string;
+  readonly MONGODB_DB: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+declare namespace App {
+  interface Locals {
+    userID: string;
+  }
+}
